82ba4347723af0bc792409c25e914b974ab60b9f
Matheus <anyonebr1@gmail.com>
2020-11-10 13:16:19 +0000

    Fixed misplaced FK on Applications table

--- database/migrations/2020_11_09_235200_create_applications_table.php
+++ database/migrations/2020_11_09_235200_create_applications_table.php
@@ -15,8 +15,8 @@ class CreateApplicationsTable extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->unsignedBigInteger('icto')->primary();
-            $table->text('description');
-            $table->foreignId('type')->constrained('server_types','server_type_id');
+            $table->string('name');
+            $table->string('short');
         });
     }
 
